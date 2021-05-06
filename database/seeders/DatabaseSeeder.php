7884fef0696348634454a1e69be784684eff46a4
Dani Vila <d.vila@tesipro.com>
2021-05-06 15:29:58 +0200

    Model relationships and database seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Project;
+use App\Models\Task;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::statement('PRAGMA foreign_keys = OFF;');
+        DB::table('project_user')->truncate();
+        Project::truncate();
+        Task::truncate();
+        User::truncate();
+        DB::statement('PRAGMA foreign_keys = ON;');
+
+        $admin = User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@projectmanager.local',
+            'password' => bcrypt('secret')
+        ]);
+
+        $user = User::create([
+            'name' => 'Raul Alvarez',
+            'email' => 'raul@projectmanager.local',
+            'password' => bcrypt('secret')
+        ]);
+
+        $project = Project::create([
+            'title' => 'Create Project Manager App',
+            'description' => 'Write the Project Manager',
+            'manager_id' => $admin->id
+        ]);
+
+        $task1 = Task::create([
+            'title' => 'Seed database',
+            'description' => 'Seed the database whit test data',
+            'status_code' => 'COMP',
+            'project_id' => $project->id,
+            'user_id' => $admin->id,
+        ]);
+
+        $task2 = Task::create([
+            'title' => 'Complete development',
+            'description' => 'Write all the code',
+            'status_code' => 'OPEN',
+            'project_id' => $project->id,
+            'user_id' => $user->id,
+        ]);
+
+        $project->users()->saveMany([$admin, $user]);
     }
 }
